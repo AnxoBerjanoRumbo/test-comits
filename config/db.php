55f9bc27226de74c06d13550d7d25e182ef1911f
AnxoBerjanoRumbo <aberjanor24@fpcoruna.afundacion.org>
2026-02-23 13:32:46 +0100

    feat: implementar conexion PDO a la base de datos

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$db_name = "ark_hub";
+$username = "root";
+$password = "";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa"
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+?>
