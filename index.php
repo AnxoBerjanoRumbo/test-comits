8e16953950ddaa212b7c319f1a814ce67c79942e
AnxoBerjanoRumbo <aberjanor24@fpcoruna.afundacion.org>
2026-02-24 15:49:09 +0100

    feat: añadir prueba de conexion visual en la pagina de inicio

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<h1>Proyecto ARK de Anxo</h1>
+<?php include 'config/db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>ARK Hub</title>
+</head>
+<body>
+    <h1>Proyecto ARK de Anxo</h1>
+    
+    <?php
+    if (isset($conexion)) {
+        echo "<p style='color: green;'>✅ Conexión con la base de datos establecida con éxito.</p>";
+    }
+    ?>
+</body>
+</html>
