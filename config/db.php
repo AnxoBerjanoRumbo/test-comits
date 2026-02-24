d5a648ffe86db5b698b07c02db0bf187cade2d64
AnxoBerjanoRumbo <aberjanor24@fpcoruna.afundacion.org>
2026-02-24 15:56:28 +0100

    fix: corregir error en la conexion a la base de datos

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ $password = "";
 try {
     $conexion = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa"
+    echo "Conexión exitosa";
 } catch(PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
