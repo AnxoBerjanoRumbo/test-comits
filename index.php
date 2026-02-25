8ea594e09a648b734d87362af358b691f17f3ea9
AnxoBerjanoRumbo <aberjanor24@fpcoruna.afundacion.org>
2026-02-25 12:55:09 +0100

    fix: corregir ruta de carga del archivo CSS

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $dinos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARK Survival Hub - Wiki</title>
-    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="assets/css/estilos.css?v=1.1">
 </head>
 <body>
     <header>
